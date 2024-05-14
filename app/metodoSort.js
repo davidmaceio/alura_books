a1d755c61aded45593199eaae98da6d2baf0c4e7
David Anderson <davidmaceio@hotmail.com>
2024-05-14 16:45:06 -0300

    Scripts atualizado

---
+++ app/metodoSort.js
@@ -0,0 +1,8 @@
+let ordenarPorPreco = document.getElementById ('btnOrdenarPorPreco')
+
+btnOrdenarPorPreco.addEventListener('click, ordenarLivrosPorPreco' )
+
+function ordenarLivrosPorPreco() {
+    let livrosOrdenados = livros.sort((a,b)=>a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
